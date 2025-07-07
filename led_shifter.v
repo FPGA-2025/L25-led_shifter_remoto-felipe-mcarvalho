3ada19c56696c82430aacfc987e5868e91c2b91f
Felipe Carvalho <felipemarinho92020@gmail.com>
2025-07-07 19:44:16 -0300

    Adicionando tarefa

--- led_shifter.v
+++ led_shifter.v
@@ -6,5 +6,22 @@ module LedShifter #(
     output  reg [7:0] leds
 );
 
-    
+    localparam ONE_SECOND = CLK_FREQ;
+    reg [31:0] counter = 0;
+
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            leds <= 8'b00011111; 
+            counter <= 0;
+        end 
+        else begin
+            if (counter >= ONE_SECOND - 1) begin
+                leds <= {leds[6:0], leds[7]}; 
+                counter <= 0;
+            end 
+            else begin
+                counter <= counter + 1;
+            end
+        end
+    end
 endmodule
